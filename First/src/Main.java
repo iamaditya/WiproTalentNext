15d807b0c75a99d35152cbd8acc7a524d8b26c34
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-17 05:15:35 -0700

    JunitProject

--- First/src/Main.java
+++ First/src/Main.java
@@ -8,6 +8,9 @@ public class Main {
         public int add(int a, int b){
             return a+b;
         }
+        public int sub(int a, int b){
+            return a -b;
+        }
     }
 
     @Test
@@ -15,6 +18,12 @@ public class Main {
         calculator c= new calculator();
 
         assertEquals("Result",5,c.add(3,2));
+    }
+    @Test
+    public void testsub(){
+        calculator c= new calculator();
+
+        assertEquals("Result",20,c.sub(100,80));
     }
     public static void main(String[] args) {
 //        testadd();
---
+++ JunitLearning/src/com/wipro/test/testStringConcat.java
@@ -0,0 +1,36 @@
+package com.wipro.test;
+
+import com.wipro.task.DailyTasks;
+import org.junit.*;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+public class testStringConcat {
+
+    @Test
+    public void teststring(){
+        DailyTasks d1 = new DailyTasks();
+
+        assertEquals("hello aditya",d1.doStringConcat("hello","aditya"));
+    }
+
+    @Test
+    public void testsortVal(){
+        int arr[] = {5,1,2,4,3};
+        int sort[] = {1,2,3,4,5};
+
+        DailyTasks d1 = new DailyTasks();
+
+        int result[] = d1.sortValues(arr);
+        assertTrue(Arrays.equals(result,sort));
+    }
+
+    @Test
+    public void testpresent(){
+        DailyTasks d1 = new DailyTasks();
+        assertTrue(d1.checkPresence("aditya sharma","sharma"));
+
+
+    }
+}
