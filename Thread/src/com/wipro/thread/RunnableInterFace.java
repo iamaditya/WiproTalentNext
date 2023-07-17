25a51eef752c88ed789a6076785d6d8b481985d4
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-17 10:34:43 -0700

    Added Multiple Threads

--- Thread/src/com/wipro/thread/RunnableInterFace.java
+++ Thread/src/com/wipro/thread/RunnableInterFace.java
@@ -9,9 +9,17 @@ public class RunnableInterFace implements Runnable{
     public static void main(String[] args) {
         RunnableInterFace runthread = new RunnableInterFace();
         Thread t1 = new Thread(runthread,"Aditya");
-
+        Thread t2 = new Thread(runthread,"second");
+        Thread t3 = new Thread(runthread,"third");
+        Thread t4 = new Thread(runthread,"fourth");
         t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
         System.out.println("Thread Name : "+t1.getName());
+        System.out.println("Thread Name : "+t2.getName());
+        System.out.println("Thread Name : "+t3.getName());
+        System.out.println("Thread Name : "+t4.getName());
     }
 
 }
